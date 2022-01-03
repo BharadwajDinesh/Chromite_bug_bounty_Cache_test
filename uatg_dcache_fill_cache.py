bf726ecaaf2d97d7809b8dc2e4cba01f16411d32
Pruthvi-Raj-S <87191910+Pruthvi-Raj-S@users.noreply.github.com>
2022-01-03 11:03:53 +0530

    Update uatg_dcache_fill_cache.py

--- uatg_dcache_fill_cache.py
+++ uatg_dcache_fill_cache.py
@@ -38,7 +38,7 @@ class uatg_cache_dcache_fill_buffer(IPlugin):
         asm='\n\tfence\n\tla t1,rvtest_data\t\n'
         
         for i in range(self._cache_size):
-	        asm+=f'\n\tlw t0, 0(t1)\n\tli a1, {self._sets*self._block_size*self._word_size}\n\taddi t1, t1, a1\n'
+	        asm+=f'\n\tlw t0, 0(t1)\n\tli a1, {self._sets*self._block_size*self._word_size}\n\tadd t1, t1, a1\n'
         
         asm+="end:\t\n\tnop\n"
 
